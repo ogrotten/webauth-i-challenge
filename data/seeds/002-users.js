765008e9078a33b6cd03f2b3319c2f3857a4362e
darrin <gordon@bleu.gordonbleu>
2019-12-16 16:42:41 -0600

    db setup complete

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -2,9 +2,9 @@ exports.seed = function (knex) {
 	// 000-cleanup.js deleted the data from all tables,
 	// we only need to insert the seed data
 
-	return knex('table_name').insert([
-		{ username: 'rowValue1', password: "pass", role_id: 3 },
-		{ username: 'rowValue2', password: "pass", role_id: 3 },
-		{ username: 'rowValue3', password: "pass", role_id: 3 }
+	return knex('users').insert([
+		{ username: 'Mike', password: "pass", role_id: 3 },
+		{ username: 'Dave', password: "pass", role_id: 3 },
+		{ username: 'James', password: "pass", role_id: 3 }
 	]);
 };
